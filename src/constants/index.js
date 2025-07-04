2d3e4e652254dfe1d11671ef35fda71e58e801bd
aatifahmed001 <aatifahmed001@gmail.com>
2025-07-04 16:21:19 +0530

    handle double slash

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
 import {getEnvironmentVariable} from '../utils/env.helpers'
 
-const serviceURL = new URL(getEnvironmentVariable('VITE_API_HOST'));
-const baseURL = serviceURL + '/userapi'
+// const serviceURL = new URL(getEnvironmentVariable('VITE_API_HOST'));
+// const baseURL = serviceURL + '/userapi'
+const serviceURL = getEnvironmentVariable('VITE_API_HOST'); // leave as string
+const baseURL = new URL('/userapi/', API_HOST).toString();
 
 console.log("🧩 VITE_API_HOST:", serviceURL);
 console.log("🧩 baseURL:", baseURL);
@@ -9,15 +11,26 @@ console.log("🧩 registerUser endpoint:", apiEndpoints.registerUser);
 
 export const apiEndpoints = {
     baseURL: baseURL,
-    auth: `${baseURL}/authenticate`,
-    loginUser: `${baseURL}/loginUser`,    // http://localhost:5000/userapi/loginUser
-    logoutUser: `${baseURL}/logoutUser`,
-    registerUser: `${baseURL}/registerUser`,
-    forgotPassword: `${baseURL}/forgotPassword`,
-    resetPasswordByToken: `${baseURL}/resetPassword/byToken`,
-    getQRLink: `${baseURL}/getQRLink`,
-    deleteQRById: `${baseURL}/deleteQR/byId`,
-    editQRById: `${baseURL}/editQR/byId`,
+    auth: new URL('authenticate', baseURL).toString(),
+    loginUser: new URL('loginUser', baseURL).toString(),
+    logoutUser: new URL('logoutUser', baseURL).toString(),
+    registerUser: new URL('registerUser', baseURL).toString(),
+    forgotPassword: new URL('forgotPassword', baseURL).toString(),
+    resetPasswordByToken: new URL('resetPassword/byToken', baseURL).toString(),
+    getQRLink: new URL('getQRLink', baseURL).toString(),
+    deleteQRById: new URL('deleteQRById', baseURL).toString(),
+    editQRById: new URL('editQRById', baseURL).toString(),
+
+    // baseURL: baseURL,
+    // auth: `${baseURL}/authenticate`
+    // loginUser: `${baseURL}/loginUser`,    // http://localhost:5000/userapi/loginUser
+    // logoutUser: `${baseURL}/logoutUser`,
+    // registerUser: `${baseURL}/registerUser`,
+    // forgotPassword: `${baseURL}/forgotPassword`,
+    // resetPasswordByToken: `${baseURL}/resetPassword/byToken`,
+    // getQRLink: `${baseURL}/getQRLink`,
+    // deleteQRById: `${baseURL}/deleteQR/byId`,
+    // editQRById: `${baseURL}/editQR/byId`,
 }
 
 export const ROUTES = {
